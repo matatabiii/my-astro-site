dfb5723d575edcbb5c640c8d239617cdab3da68d
KeishirouKawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-09-25 08:42:42 +0900

    update astro-1.3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://www.my-site.dev',
-  vite: {
-    build: {
-      cssCodeSplit: false,
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            console.log(assetInfo);
-            if (assetInfo.name == 'global.scss') return 'css/customname.css'
-            return 'css/[ext]-[name]-[hash][extname]'
-          },
-          compact: true
-        },
-      },
-    },
-  },
-})
+export default defineConfig({});

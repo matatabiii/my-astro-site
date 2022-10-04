87c4325751e75f52867fd7110b331348e75c8600
KeishirouKawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-10-04 22:07:43 +0900

    update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+const base = {
+  development: '/', // 開発環境用のパス
+  production: '/example' // 本番環境用のパス
+}
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: `https://example.com${base.production}`,
+  base: import.meta.env.DEV ? base.development : base.production,
+  outDir: `.${base.production}`,
+  server: ({ command }) => ({ port: command === 'dev' ? 3000 : 4000 }),
+})

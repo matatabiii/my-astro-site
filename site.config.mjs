8da79acf131c22bcfe16782e5cfe106b67c3be24
KeishirouKawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-11-12 13:52:02 +0900

    tsconfig設定更新

---
+++ site.config.mjs
@@ -0,0 +1,17 @@
+export const base = {
+  development: '/', // 開発環境用のパス
+  production: '/' // 本番環境用のパス
+}
+
+export const siteConfig = {
+  siteName: 'サイト名',
+  siteUrl: `https://example.com`,
+  base: base,
+  ogImage: 'og-image.jpg',
+  msapplicationTileColor: '#ffffff',
+  themeColor: '#ffffff',
+  charset: 'UTF-8',
+  locale: 'ja_JP',
+  favicon: 'favicon.png',
+  appleTouchIcon: 'apple-touch-icon.png',
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,32 @@
-const detailsNodes = document.querySelectorAll('details')
+// const detailsNodes = document.querySelectorAll('details')
 
-const calcHeight = (details, summary, detailsContent) => {
-  return !details.open ? summary.scrollHeight : summary.scrollHeight + detailsContent.scrollHeight
-}
+// const calcHeight = (details, summary, detailsContent) => {
+//   return !details.open ? summary.scrollHeight : summary.scrollHeight + detailsContent.scrollHeight
+// }
 
-if (detailsNodes.length) {
-  detailsNodes.forEach((details) => {
-    const summary = details.querySelector('summary')
-    const detailsContent = details.querySelector('.c-details__content')
+// if (detailsNodes.length) {
+//   detailsNodes.forEach((details) => {
+//     const summary = details.querySelector('summary')
+//     const detailsContent = details.querySelector('.c-details__content')
 
-    summary.addEventListener('click', (event) => {
-      event.preventDefault()
+//     summary.addEventListener('click', (event) => {
+//       event.preventDefault()
 
-      // クリック時の初期値設定
-      details.style.height = `${calcHeight(details, summary, detailsContent)}px`
+//       // クリック時の初期値設定
+//       details.style.height = `${calcHeight(details, summary, detailsContent)}px`
 
-      // opne
-      details.open = !details.open
+//       // opne
+//       details.open = !details.open
 
-      // その後の値設定
-      details.style.height = `${calcHeight(details, summary, detailsContent)}px`
+//       // その後の値設定
+//       details.style.height = `${calcHeight(details, summary, detailsContent)}px`
 
-      // アニメーション終了時にautoに
-      details.ontransitionend = (event) => {
-        if (event.propertyName === 'height') details.style.height = ''
-      }
+//       // アニメーション終了時にautoに
+//       details.ontransitionend = (event) => {
+//         if (event.propertyName === 'height') details.style.height = ''
+//       }
 
-      details.open ? details.classList.add('is-open') : details.classList.remove('is-open')
-    })
-  })
-}
+//       details.open ? details.classList.add('is-open') : details.classList.remove('is-open')
+//     })
+//   })
+// }

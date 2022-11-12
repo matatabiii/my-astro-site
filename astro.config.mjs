eeafd8cb3679331868bd34d39051cc8f9cafd7ae
KeishirouKawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-11-12 14:55:18 +0900

    build時の出力先変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
-import { defineConfig } from 'astro/config';
-import { base, siteConfig } from './site.config.mjs';
+import { defineConfig } from 'astro/config'
+import { base, siteConfig } from './site.config.mjs'
 
 // https://astro.build/config
 export default defineConfig({
   site: `${siteConfig.siteUrl}${base.production}`,
   base: import.meta.env.DEV ? base.development : base.production,
   server: ({ command }) => ({ port: command === 'dev' ? 3000 : 4000 }),
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          // entry chunk assets それぞれの書き出し名の指定
+          entryFileNames: `assets/js/main.js`,
+          chunkFileNames: `assets/js/chunks/[name].js`,
+          assetFileNames: `assets/[ext]/[name][extname]`,
+        },
+      },
+    },
+  },
 })
--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,9 @@
-// const detailsNodes = document.querySelectorAll('details')
+console.log(document.title);
 
-// const calcHeight = (details, summary, detailsContent) => {
-//   return !details.open ? summary.scrollHeight : summary.scrollHeight + detailsContent.scrollHeight
-// }
-
-// if (detailsNodes.length) {
-//   detailsNodes.forEach((details) => {
-//     const summary = details.querySelector('summary')
-//     const detailsContent = details.querySelector('.c-details__content')
-
-//     summary.addEventListener('click', (event) => {
-//       event.preventDefault()
-
-//       // クリック時の初期値設定
-//       details.style.height = `${calcHeight(details, summary, detailsContent)}px`
-
-//       // opne
-//       details.open = !details.open
-
-//       // その後の値設定
-//       details.style.height = `${calcHeight(details, summary, detailsContent)}px`
-
-//       // アニメーション終了時にautoに
-//       details.ontransitionend = (event) => {
-//         if (event.propertyName === 'height') details.style.height = ''
-//       }
-
-//       details.open ? details.classList.add('is-open') : details.classList.remove('is-open')
-//     })
-//   })
-// }
+const detailsNodes = document.querySelectorAll('details')
+if (detailsNodes.length) {
+  (async () => {
+    const { detailsUi } = await import('./modules/details.js')
+    detailsUi(detailsNodes)
+  })()
+}

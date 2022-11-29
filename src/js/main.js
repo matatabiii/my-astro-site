a7ab1bc835cbe979f84f6deab58aeb33aaee38e0
KeishirouKawaguchi <27771489+matatabiii@users.noreply.github.com>
2022-11-29 22:22:46 +0900

    レイアウト・ロジック構築中

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
+import { drawer } from './modules/drawer.js'
+
 const detailsNodes = document.querySelectorAll('details')
 if (detailsNodes.length) {
   (async () => {
     const { detailsUi } = await import('./modules/details.js')
     detailsUi(detailsNodes)
   })()
+}
+
+const toggleElement = document.getElementById('menu-toggle')
+if (toggleElement) {
+  drawer(toggleElement)
 }
---
+++ src/js/modules/drawer.js
@@ -0,0 +1,95 @@
+export const drawer = (toggleElement) => {
+  const scrollable = typeof document.scrollingElement !== 'undefined' ? document.scrollingElement : document.documentElement
+  const controlsId = toggleElement.getAttribute('aria-controls') // トグルの操作対象要素のid
+  const drawerElement = document.getElementById(controlsId) // トグルの操作対象要素（ドロワーメニュー自体）
+
+  // 背景スクロールロック
+  const activateScrollLock = () => {
+    const visibleWidth = scrollable.clientWidth
+    document.documentElement.style.overflow = 'hidden'
+    const hiddenWidth = scrollable.clientWidth
+    const scrollbarWidth = hiddenWidth - visibleWidth
+    // document.documentElement.classList.add(this.config.stateClassName)
+
+    if (scrollbarWidth) {
+      scrollable.style.paddingRight = scrollbarWidth + 'px'
+
+      // if (this.calcScrollbarElm) {
+      //   document.querySelectorAll(this.config.calcScrollbarElmsName).forEach((calcScrollbarElm) => {
+      //     console.log(calcScrollbarElm)
+      //     calcScrollbarElm.style.paddingRight = scrollbarWidth + 'px'
+      //   })
+      // }
+    }
+  }
+
+  const deactivateScrollLock = () => {
+    document.documentElement.style.removeProperty('overflow')
+    scrollable.style.removeProperty('padding-right')
+    // document.documentElement.classList.remove(this.config.stateClassName)
+  }
+
+  const isOpenToggleDrawer = () => {
+    const isOpenToggle = toggleElement.getAttribute('aria-expanded') === 'true' // トグルの状態がopenかcloseか
+    const isOpenDrawer = drawerElement.getAttribute('aria-hidden') !== 'true' // ドロワーの状態がopenかcloseか
+    return isOpenToggle && isOpenDrawer
+  }
+
+  const setOpen = () => {
+    toggleElement.setAttribute('aria-expanded', 'true') // aria-expanded=true => open
+    drawerElement.setAttribute('aria-hidden', 'false') // aria-hidden=false => open
+    toggleElement.setAttribute('aria-label', 'close')
+    activateScrollLock()
+    drawerElement.style.visibility = 'visible'
+  }
+
+  const setClose = () => {
+    toggleElement.setAttribute('aria-expanded', 'false') // aria-expanded=true => open
+    drawerElement.setAttribute('aria-hidden', 'true') // aria-hidden=false => open
+    toggleElement.setAttribute('aria-label', 'menu')
+    drawerElement.style.visibility = 'hidden'
+  }
+
+  const toggle = () => {
+    const isOpen = isOpenToggleDrawer()
+    isOpen ? setClose() : setOpen()
+  }
+
+  const onToggle = () => {
+    toggle()
+  }
+
+  // Escapeキーでドロワーを閉じる
+  const onKeydownEsc = (event) => {
+    if (!isOpenToggleDrawer() || event.key !== 'Escape') {
+      return
+    }
+    event.preventDefault()
+    setClose()
+  }
+
+  // スクロールロック解除はtransitionEventを監視
+  const onTransitionendDrawer = (event) => {
+    if (event.target !== drawerElement || event.propertyName !== 'visibility') {
+      return
+    }
+    if (!isOpenToggleDrawer()) {
+      deactivateScrollLock()
+    }
+  }
+
+  setClose() // 初期化（閉じる）
+
+  toggleElement.addEventListener('click', (event) => {
+    event.preventDefault()
+    onToggle()
+  })
+
+  window.addEventListener('keydown', (event) => {
+    onKeydownEsc(event)
+  }, false)
+
+  drawerElement.addEventListener('transitionend', (event) => {
+    onTransitionendDrawer(event)
+  }, false)
+}
